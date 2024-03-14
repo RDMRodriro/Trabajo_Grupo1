88f4fe495dbae7f1a68455ba56ccd63bac4b82e4
valtis1405 <valtis1405@gmail.com>
2024-03-14 11:15:50 -0600

    tilin

--- Assets/Ciudad/Diego/escuela/JMO Assets/Welcome Screen/CFXR_WelcomeScreen.cs
+++ Assets/Ciudad/Diego/escuela/JMO Assets/Welcome Screen/CFXR_WelcomeScreen.cs
@@ -42,7 +42,7 @@ namespace CartoonFX
 
             // UXML
             var uxmlDocument = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(AssetDatabase.GUIDToAssetPath("bfd03f272fe010b4ba558a3bc456ffeb"));
-            root.Add(uxmlDocument.Instantiate());
+        
             // USS
             var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(AssetDatabase.GUIDToAssetPath("f8b971f10a610844f968f582415df874"));
             root.styleSheets.Add(styleSheet);
