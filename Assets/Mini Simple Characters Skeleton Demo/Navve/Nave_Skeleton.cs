6cf7b4f0a93ecdcbcae1b07acebe994767735072
RDMRodriro <153305084+RDMRodriro@users.noreply.github.com>
2024-06-11 12:10:28 -0600

    skeleton

--- Assets/Mini Simple Characters Skeleton Demo/Navve/Nave_Skeleton.cs
+++ Assets/Mini Simple Characters Skeleton Demo/Navve/Nave_Skeleton.cs
@@ -1,18 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Nave_Skeleton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public NavMeshAgent Agent;
+    public Transform pointer;
+    public float LookRadius = 80f;
+
+    //Animación
+    public Animator animator;
+    public float DistanceShoot = 30f;
+    //Bala
+    //public GameObject Bala;
+    public Transform PointerEspada;
+    //vida
+    public int vida = 40;
+
     void Start()
     {
-        
+        Agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        MovimientoSkeleton();
+    }
+
+    public void MovimientoSkeleton()
+    {
+        float distancia = Vector3.Distance(pointer.position, transform.position);
+        if(distancia <= LookRadius)
+            {
+                FaceTarget();
+                Agent.SetDestination(pointer.position);
+                animator.SetBool("Correr", true);
+                Agent.speed = 5f;
+
+                if(distancia<= DistanceShoot)
+                {
+                    animator.SetBool("Ataque", true);
+                    Agent.speed = 0;
+                }
+                else
+                {
+                    animator.SetBool("Ataque", false);
+                }
+            }
+        else
+            {
+                animator.SetBool("Correr", false);
+                Agent.speed = 0;
+            } 
+        Debug.Log(distancia);
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, LookRadius);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, DistanceShoot);
+    }
+
+
+    void FaceTarget ()
+    {
+        Vector3 direction = (pointer.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 1f);
+    }
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            vida = vida - 20;
+            if (vida == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
