8c2054ebf3e34d7451161118a1899555dd992f3f
Caterpie667 <juanxfpl@gmail.com>
2024-06-12 21:13:55 -0600

    uwu

---
+++ Assets/TpMenuInicio.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpMenuInicio : MonoBehaviour
+{
+    public void MenuInicial()
+    {
+        if ()
+    }
+}
