4fa333a2a9eae899e1f6b781dc6ca6e0eab69eb9
valtis1405 <valtis1405@gmail.com>
2024-06-26 10:42:54 -0600

    juegoTerminado

---
+++ Assets/loki/Cubo_final_juego.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cubo_final_juego : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
